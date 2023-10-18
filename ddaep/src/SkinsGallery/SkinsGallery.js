04f8209f8bc7ba7e2d6adc847e0cb7c68f3e7450
Mykolas Perevicius <mykolas.perevicius@projectinnovatenewark.org>
2023-10-18 16:43:54 -0400

    second

--- ddaep/src/SkinsGallery/SkinsGallery.js
+++ ddaep/src/SkinsGallery/SkinsGallery.js
@@ -4,15 +4,14 @@ import './SkinsGallery.css';
 const skinsData = [
     {
         name: "AWP | Gungnir",
+        suggestedPrice: "$14,633.18",
         price: "$12,750.00",
-        image: "https://dgvdyislmj77y.cloudfront.net/eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJpbWFnZXMvaXRlbXMvYXdwLWd1bmduaXItLWZhY3RvcnktbmV3LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTIwMCwiaGVpZ2h0IjoxMjAwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6IiMyNzI3M2YifSwiZmxhdHRlbiI6eyJiYWNrZ3JvdW5kIjoiIzI3MjczZiJ9LCJvdmVybGF5V2l0aCI6eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJ3YXRlcm1hcmstbG9nby5wbmciLCJvcHRpb25zIjp7ImdyYXZpdHkiOiJzb3V0aHdlc3QifX19fQ=="
+        discount: "-13%",
+        image: "https://dgvdyislmj77y.cloudfront.net/eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJpbWFnZXMvaXRlbXMvYXdwLWd1bmduaXItLWZhY3RvcnktbmV3LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTIwMCwiaGVpZ2h0IjoxMjAwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6IiMyNzI3M2YifSwiZmxhdHRlbiI6eyJiYWNrZ3JvdW5kIjoiIzI3MjczZiJ9LCJvdmVybGF5V2l0aCI6eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJ3YXRlcm1hcmstbG9nby5wbmciLCJvcHRpb25zIjp7ImdyYXZpdHkiOiJzb3V0aHdlc3QifX19fQ==",
+        type: "Factory New Covert Sniper Rifle",
+        float: "0.066",
     },
-    {
-        name: "Butterfly Knife | Lore",
-        price: "$3,002.61",
-        image: "https://dgvdyislmj77y.cloudfront.net/eyJidWNrZXQiOiJ0cmFkZWl0LXdpa2kiLCJrZXkiOiJpbWFnZXMvaXRlbXMvYnV0dGVyZmx5LWtuaWZlLWxvcmUtLWZhY3RvcnktbmV3LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTIwMCwiaGVpZ2h0Ijo5MDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjoiIzI3MjczZiJ9LCJmbGF0dGVuIjp7ImJhY2tncm91bmQiOiIjMjcyNzNmIn0sIm92ZXJsYXlXaXRoIjp7ImJ1Y2tldCI6InRyYWRlaXQtd2lraSIsImtleSI6IndhdGVybWFyay1sb2dvLnBuZyIsIm9wdGlvbnMiOnsiZ3Jhdml0eSI6InNvdXRod2VzdCJ9fX19"
-    },
-    // ... add all other skins here
+    // ... add all other skins similarly
 ];
 
 function SkinsGallery() {
@@ -20,9 +19,20 @@ function SkinsGallery() {
         <div className="skinsGallery">
             {skinsData.map((skin, index) => (
                 <div key={index} className="skinCard">
-                    <img src={skin.image} alt={skin.name} />
-                    <p>{skin.name}</p>
-                    <p>{skin.price}</p>
+                    <div className="skinImageContainer">
+                        <img src={skin.image} alt={skin.name} />
+                        <span className="tradeableBadge">Tradable</span>
+                    </div>
+                    <div className="skinInfo">
+                        <h2 className="skinPrice">{skin.price}</h2>
+                        <p className="suggestedPrice">{skin.suggestedPrice}</p>
+                        <h1 className="skinName">{skin.name}</h1>
+                        <p className="skinType">{skin.type}</p>
+                        <div className="skinFloatBar">
+                            <div className="currentFloat" style={{width: `${parseFloat(skin.float) * 100}%`}}></div>
+                        </div>
+                        <p className="skinFloatValue">{skin.float}</p>
+                    </div>
                 </div>
             ))}
         </div>
